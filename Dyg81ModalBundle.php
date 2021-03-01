c4c2428798280da4811ab75ea7bc2cd4d0ac3a90
Donald Yañez González <donald.yanez@gmail.com>
2021-03-01 17:28:31 -0500

    Update namespace and class. Also changed the file name

---
+++ Dyg81ModalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dyg81\ModalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Dyg81ModalBundle extends Bundle
+{
+}
--- RaresModalBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rares\ModalBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RaresModalBundle extends Bundle
-{
-}
